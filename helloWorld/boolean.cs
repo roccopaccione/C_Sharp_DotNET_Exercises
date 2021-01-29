d573cc908117e6959774770b4248d7249e8164e4
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2021-01-29 17:04:04 -0600

    feat: added boolean file

---
+++ helloWorld/boolean.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace basics
+{
+    public class boolean
+    {
+        public boolean()
+        {
+            bool isTrue = true;
+
+            bool isFull = false;
+
+            Console.WriteLine(isTrue);
+            Console.WriteLine(isFull);
+        }
+    }
+}
